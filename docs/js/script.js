6fcdd0cb64aca36c5e0961d652558c0913442f9f
BluishCode <gavincloyd8@gmail.com>
2026-06-28 03:45:20 +0000

    adding js folder and redirecting pages

---
+++ docs/js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Site loaded");
+});
+
+const burgerBtn = document.getElementById("burgerBtn");
+const navMenu = document.getElementById("navMenu");
+
+burgerBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+const memberBtn = document.getElementById("memberBtn");
+
+if (memberBtn){
+    memberBtn.addEventListener("click", () => {
+        window.location.href = 'https://www.paypal.com/webapps/hermes?token=7VK90414HM201205D&useraction=commit&wpsFlowRedirectToXorouterSkipHermesStartTime=1782234834794&flowType=WPS&mfid=1782234834579_f85554244d478';
+    });}
+
+const loginForm = document.getElementById("loginForm");
+    if(loginForm){
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        console.log("Email:", email);
+        console.log("Password:", password)
+    });}
